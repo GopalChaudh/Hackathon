92369df5f212a65a6bbc29dfd3a8c3fa9428b130
Harjaspal Singh <harjassidhu1@gmail.com>
2024-03-23 00:39:54 +0530

    new frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,18 @@
-import './App.css';
-import HomePageLayout from './layouts/homePageLayout';
+import Home from "pages/Home";
+import NavBar from "./components/NavBar";
+import NotFound from "pages/NotFound";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-const posts = [
-    {
-        key: 1,
-        title: "Post 1",
-        body: "This is the body of post1",
-        user: "User 1",
-        date: "2021-01-01",
-        category: "General",
-        likes: 10,
-        comments: 5
-    },
-    {
-        key: 2,
-        title: "Post 2",
-        body: "This is the body of post2",
-        user: "User 2",
-        date: "2021-01-02",
-        category: "General",
-        likes: 20,
-        comments: 10
-    }
-];
-
-const events = [
-    {
-        key: 1,
-        title: "Event 1",
-        body: "This is the body of event1",
-        org: "User 1",
-        date: "2021-01-01",
-        category: "General",
-        location: "Location 1",
-    },
-    {
-        key: 2,
-        title: "Event 2",
-        body: "This is the body of event2",
-        org: "User 2",
-        date: "2021-01-02",
-        category: "General",
-        location: "Location 2",
-    }
-];
-
-function App() {
+export default function App() {
   return (
-    <HomePageLayout posts={posts} events={events} />
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
